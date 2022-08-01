6416b2b6e6b0fced6c47e4989abe32f9f396e236
tomf247 <tomfoley247@gmail.com>
2022-08-01 13:43:20 +0000

    CRUD operations

--- sql-expression.py
+++ sql-expression.py
@@ -44,7 +44,7 @@ with db.connect() as connection:
     # select_query = artist_table.select()
 
     # Query 2 - select only the "Name" column from the "Artist" table
-    select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
 
     # Query 3 - select only 'Queen' from the "Artist" table
     # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
@@ -53,7 +53,7 @@ with db.connect() as connection:
     # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
 
     # Query 5 - select only the albums with 'ArtistId' #51 on the "Album" table
-    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+    select_query = album_table.select().where(album_table.c.ArtistId == 51)
 
     # Query 6 - select all tracks where the composer is 'Queen' from the "Track" table
     # select_query = track_table.select().where(track_table.c.Composer == "Queen")
