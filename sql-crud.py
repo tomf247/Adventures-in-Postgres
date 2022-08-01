53eba1505102b0551d85d1c1f0abb7e8bbba5731
tomf247 <tomfoley247@gmail.com>
2022-08-01 13:45:44 +0000

    Fix indentation

--- sql-crud.py
+++ sql-crud.py
@@ -89,16 +89,16 @@ tom_foley = Programmer(
 )
 
 # add each instance of our programmers to our session
-#session.add(ada_lovelace)
-#session.add(alan_turing)
-#session.add(grace_hopper)
-#session.add(margaret_hamilton)
-#session.add(bill_gates)
-#session.add(tim_berners_lee)
-#session.add(tom_foley)
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+# session.add(tom_foley)
 
 # commit our session to the database
-#session.commit()
+# session.commit()
 
 
 # updating a single record
@@ -119,22 +119,22 @@ tom_foley = Programmer(
 #         print("Gender not defined")
 #     session.commit()
 
-# deleting a single record
-fname = input("Enter a first name: ")
-lname = input("Enter a last name: ")
-programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
-# defensive programming
-if programmer is not None:
-    print("Programmer found: ", programmer.first_name + " " + programmer.last_name)
-    confirmation = input("Are you sure you want to delete this record? (y/n) ")
-    if confirmation.lower() == "y":
-        session.delete(programmer)
-        session.commit
-        print("Programmer " + fname + " " + lname + "  deleted")
-    else:
-        print("Programmer " + fname + " " + lname + "  was not deleted")
-else:
-    print("No records found")
+# # deleting a single record
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# # defensive programming
+# if programmer is not None:
+#     print("Programmer found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit
+#         print("Programmer " + fname + " " + lname + "  deleted")
+#     else:
+#         print("Programmer " + fname + " " + lname + "  was not deleted")
+# else:
+#     print("No records found")
 
 
 # query the database to find all Programmers
